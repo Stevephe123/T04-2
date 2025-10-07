aa8288a8b4f9a1f00caa491227088e4440a12202
horngenpo@gmail.com <145650881+Steve102779593@users.noreply.github.com>
2025-10-08 02:26:02 +0800

    Update energy-d3.js

--- energy-d3.js
+++ energy-d3.js
@@ -4,8 +4,8 @@ d3.select("h1")
 
 // append a paragraph inside the div
 d3.select("#content")
-    .append("p")
-    .text("Purchasing a low energy consumption TV will help with your energy bills!");
+  .append("p")
+  .text("Purchasing a low energy consumption TV will help with your energy bills!");
 
 // append an empty rect (invisible)
 d3.select("svg")
