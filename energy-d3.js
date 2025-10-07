e4337ceca169e9b8e086431b4ce45526c7332061
horngenpo@gmail.com <145650881+Steve102779593@users.noreply.github.com>
2025-10-08 02:21:23 +0800

    Create energy-d3.js

---
+++ energy-d3.js
@@ -0,0 +1,21 @@
+// style the h1
+d3.select("h1")
+    .style("color", "green");
+
+// append a paragraph inside the div
+d3.select("#content")
+    .append("p")
+    .text("Purchasing a low energy consumption TV will help with your energy bills!");
+
+// append an empty rect (invisible)
+d3.select("svg")
+    .append("rect");
+
+// append a visible rect
+d3.select("svg")
+    .append("rect")
+    .attr("x", 50)
+    .attr("y", 50)
+    .attr("width", 100)
+    .attr("height", 30)
+    .style("fill", "green");
